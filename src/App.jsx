91f3e2a3f0f9de14944e0116b3b2189efb79dde5
Brandon Yahir Escobedo Rodriguez <brandon.escobedo@crediclub.com>
2025-10-22 13:26:37 -0600

    d

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Box } from '@mui/material';
 
 import MainLayout from './components/MainLayout';
@@ -16,24 +16,20 @@ import ReportsPage from './pages/ReportsPage';
 function App() {
   return (
     <Box sx={{ bgcolor: '#f5f5f5', minHeight: '100vh' }}>
-      <HashRouter>
-        <Routes>
-          {/* Rutas públicas */}
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/medical-form" element={<MedicalFormPage />} />
-          <Route path="login" element={<LoginPage />} />
-          <Route path="register" element={<RegisterPage />} />
+   <BrowserRouter basename={import.meta.env.BASE_URL || '/'}>
+  <Routes>
+    <Route path="/" element={<LandingPage />} />
+    <Route path="/medical-form" element={<MedicalFormPage />} />
 
-          {/* Rutas protegidas / dashboard */}
-          <Route element={<MainLayout />}>
-            <Route path="dashboard" element={<DashboardPage />} />
-            <Route path="patients" element={<PatientsPage />} />
-            <Route path="patients/:patientId" element={<PatientRecordPage />} />
-            <Route path="appointments" element={<AppointmentsPage />} />
-            <Route path="reports" element={<ReportsPage />} />
-          </Route>
-        </Routes>
-      </HashRouter>
+    <Route path="/dashboard" element={<MainLayout />}>
+      <Route index element={<DashboardPage />} />
+      <Route path="patients" element={<PatientsPage />} />
+      <Route path="patients/:patientId" element={<PatientRecordPage />} />
+      <Route path="appointments" element={<AppointmentsPage />} />
+      <Route path="reports" element={<ReportsPage />} />
+    </Route>
+  </Routes>
+</BrowserRouter>
     </Box>
   );
 }
