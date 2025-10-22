869bb8b8b1aad5f028fc1bf328a843ea47731e59
Brandon Yahir Escobedo Rodriguez <brandon.escobedo@crediclub.com>
2025-10-22 12:18:38 -0600

    c

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base:'/DoctorsOnline_FrontEnd2/'
+  base:"/DoctorsOnline_FrontEnd2",
+  plugins: [react()]
 })
